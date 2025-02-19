c5622edd7335e7b6689cfa67bb39de189fe3884f
Yinghao Qin <yh.qin@foxmail.com>
2025-02-19 15:09:27 +0000

    add stats interface

---
+++ include/stats_interface.hpp
@@ -0,0 +1,53 @@
+//
+// Created by Yinghao Qin on 19/02/2025.
+//
+
+#ifndef FROGS_STATS_INTERFACE_HPP
+#define FROGS_STATS_INTERFACE_HPP
+
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <chrono>
+#include <filesystem>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <numeric>
+
+namespace fs = std::filesystem;
+using namespace std;
+
+struct Indicators {
+    double min{};
+    double max{};
+    double avg{};
+    double std{};
+    std::size_t size{};
+};
+
+const std::string kStatsPath = "../stats";
+
+class StatsInterface {
+public:
+    std::chrono::high_resolution_clock::time_point start;
+    std::chrono::duration<double> duration;
+
+    std::ofstream log_evolution;
+    std::ostringstream oss_row_evol;
+    std::ofstream log_solution;
+
+    static Indicators calculate_statistical_indicators(const std::vector<double>& data) ;
+    static bool create_directories_if_not_exists(const std::string& director_path);
+    static void stats_for_multiple_trials(const std::string& file_path, const std::vector<double>& data); // open a file, save the statistical info, and then close it
+    virtual void open_log_for_evolution() = 0; // open a file
+    virtual void flush_row_into_evol_log() = 0; // flush the evolution info into the file
+    virtual void close_log_for_evolution() = 0; // close the file
+    virtual void save_log_for_solution() = 0; // open a file, save the solution, and close it
+
+    virtual ~StatsInterface() = default;
+};
+
+#endif //FROGS_STATS_INTERFACE_HPP
---
+++ src/stats_interface.cpp
@@ -0,0 +1,61 @@
+//
+// Created by Yinghao Qin on 19/02/2025.
+//
+
+#include "stats_interface.hpp"
+
+Indicators StatsInterface::calculate_statistical_indicators(const std::vector<double>& data) {
+    Indicators indicators;
+
+    if (data.empty()) return indicators;
+
+    indicators.size = data.size();
+    indicators.min = *std::min_element(data.begin(), data.end());
+    indicators.max = *std::max_element(data.begin(), data.end());
+    double sum = std::accumulate(data.begin(), data.end(), 0.0);
+    indicators.avg = sum / static_cast<double>(indicators.size);
+
+    if (indicators.size == 1) {
+        indicators.std = 0.0;
+    } else {
+        double variance = std::accumulate(data.begin(), data.end(), 0.0,
+                                          [indicators](double accum, double value) {
+                                              return accum + (value - indicators.avg) * (value - indicators.avg);
+                                          }) / static_cast<double>(indicators.size - 1);
+        indicators.std = std::sqrt(variance);
+    }
+
+    return indicators;
+}
+
+bool StatsInterface::create_directories_if_not_exists(const string &directory_path) {
+    if (!fs::exists(directory_path)) {
+        try {
+            fs::create_directories(directory_path);
+            return true;
+        } catch (const std::exception& e) {
+            std::cerr << "Error creating directory: " << e.what() << std::endl;
+            return false;
+        }
+    } else {
+        return true;
+    }
+}
+
+void StatsInterface::stats_for_multiple_trials(const std::string& file_path, const std::vector<double>& data) {
+    std::ofstream log_stats;
+
+    log_stats.open(file_path);
+
+    std::ostringstream oss;
+    for (auto& perf:data) {
+        oss << fixed << setprecision(2) << perf << endl;
+    }
+    Indicators indicators = calculate_statistical_indicators(data);
+    oss << "Mean " << indicators.avg << "\t \tStd Dev " << indicators.std << "\t " << endl;
+    oss << "Min: " << indicators.min << "\t " << endl;
+    oss << "Max: " << indicators.max << "\t " << endl;
+    log_stats << oss.str() << flush;
+
+    log_stats.close();
+}
