b4348e08c7edd9b07b8db2062b0d341971744cd4
Yinghao Qin <yh.qin@foxmail.com>
2025-03-28 15:31:21 +0000

    add M1,8,9 inter with empty route

--- src/leader_array.cpp
+++ src/leader_array.cpp
@@ -5,7 +5,7 @@
 
 LeaderArray::LeaderArray(int seed_val, Case *instance, Preprocessor *preprocessor)
 : random_engine(seed_val), instance(instance), preprocessor(preprocessor) {
-    this->uniform_int_dis = std::uniform_int_distribution<int>(0, 5); // 6 moves
+    this->uniform_int_dis = std::uniform_int_distribution<int>(0, 8); // 9 moves
 
     this->max_search_depth = 10;
     this->route_cap = preprocessor->route_cap_;
@@ -59,6 +59,18 @@ void LeaderArray::neighbour_explore(const double& history_val) {
             break;
         case 5:
             node_exchange_inter_for_individual();
+            break;
+        case 6:
+            perform_inter_move_with_empty_route([this](int* route1, int* route2, int& length1, int& length2, int& loading1, int& loading2)
+                {return move1_inter_with_empty_route(route1, route2, length1, length2, loading1, loading2);});
+            break;
+        case 7:
+            perform_inter_move_with_empty_route([this](int* route1, int* route2, int& length1, int& length2, int& loading1, int& loading2)
+                {return move8_inter_with_empty_route(route1, route2, length1, length2, loading1, loading2);});
+            break;
+        case 8:
+            perform_inter_move_with_empty_route([this](int* route1, int* route2, int& length1, int& length2, int& loading1, int& loading2)
+                {return move9_inter_with_empty_route(route1, route2, length1, length2, loading1, loading2);});
             break;
     }
 }
